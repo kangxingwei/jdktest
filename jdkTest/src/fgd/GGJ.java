54f0b532b54a3f53c6f62c1de085bb5de0af24ab
kang <254882274@qq.com>
2018-08-20 22:19:15 +0800

    sdf

---
+++ jdkTest/src/fgd/GGJ.java
@@ -0,0 +1,24 @@
+package fgd;
+
+public class GGJ {
+
+	public static void main(String[] args) {
+		/*sdfjsdfh
+		 * sdfksdfjl
+		 * dsfsdf
+		 * dsfsdf
+		 * dsfsdf
+		 * dsfsd
+		 * fsdf
+		 * sdfsd
+		 * f
+		 * gfghfgh
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 */
+		
+	}
+}
---
+++ jdkTest/src/fgd/KKK.java
@@ -0,0 +1,8 @@
+package fgd;
+
+public class KKK {
+
+	public static void main(String[] args) {
+		//sdfsdfsdfdsf
+	}
+}
